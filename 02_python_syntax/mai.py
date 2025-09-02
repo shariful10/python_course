2f124b6399d35429f1b6b4cb5122e0f5cb0c466e
shariful10 <freelancershariful10@gmail.com>
2025-09-02 14:40:42 +0600

    Add initial Python scripts for course setup and syntax demonstration

---
+++ 02_python_syntax/mai.py
@@ -0,0 +1,15 @@
+print("Hello, Syntax!")
+print(3 + 5)
+
+# a = 4
+# b = 2
+# if(a > 3):
+#   print("a is greater than 3")
+# if(b > 1):
+#   print("b is greater than 1")
+
+#   if(a < b):
+#     print(True)
+#   else:
+#     print(False)
+
